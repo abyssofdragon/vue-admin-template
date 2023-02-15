644f014b929ae6981b100881bef28214fb4dcfc5
jiangzy <jiangzy@coscon.com>
2023-02-15 17:35:35 +0800

    0.54 监管者信件差4个完工（补丁）

--- src/views/nested/menu1/note/hunter/components/26haoshouwei/index.vue
+++ src/views/nested/menu1/note/hunter/components/26haoshouwei/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h5>隐士</h5>
+    <h5>26号守卫</h5>
     <el-row :gutter="12">
       <Letter :letter="letterCN" />
       <Deduction :deduction="deductionCN" />
@@ -78,31 +78,31 @@ export default {
       letterCN: [
         {
           id: 'I',
-          year: '2020',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
+          year: '2021',
+          title: '一封包裹上的附信',
+          letter: '尊敬的帕杜拉先生:\n您好!\n关于您希望提升延时装置精度的需求，我实在独木难支，于是将样本拿给了我的女儿一同研究，请您见谅——我应该不只一次提过，她已经成为了超越我的天才。\n但即便如此，我们依旧只能将装置的精度在确保长时间运转的情况下提升百分之十二。钟表结构简单，功能单一，只需要讲求每一分每一秒的精细，但这个机械装置的连携功能似乎远不止于此。根据我女儿的猜测，它甚至可能是某个自主校准系统里的一个环节？——我们无意窥探您的发明，但这个想法实在令我们匪夷所思，“机械”与“自主”两个词究竟要怎样才能联系在一起？\n改造后的装置将随信送达。能为您伟大的发明尽一点微薄之力，我们倍感荣幸。\n另，虽然唐突，但我们是否有机会能够就自主机械这个方向有进一步的交流？我的女儿对此尤其痴迷。\n祝您一切顺利!\n\n忠诚的，\n列兹尼克'
         },
         {
           id: 'II',
-          year: '2021',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
+          year: '2022',
+          title: '一页日记',
+          letter: '今天订购了一批新的零件，用于改造26号。\n自从那次事件后，26号从未出现不受控制的现象，这是第一次。\n一定是那两个小鬼!\n机器人绝不可能有自主意识。\n 那个小男孩看起来更关心那些机关，那个小女孩……对！准是她趁我不注意的时候，对26号做了什么手脚。原本以为她的机器人报废后，这家伙会消停一段时间。\n我想我该重新看看26号的录像，或许有什么被我忽视掉的细节。\n……\n不，我应该先对这个蠢透了的机器下达终止指令！'
         },
         {
           id: 'III',
-          year: '2022',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
-        },
-        {
-          id: 'IV',
           year: '2023',
           title: 'Coming Soon',
           letter: 'Coming Soon'
         },
         {
-          id: 'V',
+          id: 'IV',
           year: '2024',
+          title: 'Coming Soon',
+          letter: 'Coming Soon'
+        },
+        {
+          id: 'V',
+          year: '2025',
           title: 'Coming Soon',
           letter: 'Coming Soon'
         }
--- src/views/nested/menu1/note/hunter/components/diaokejia/index.vue
+++ src/views/nested/menu1/note/hunter/components/diaokejia/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h5>隐士</h5>
+    <h5>雕刻家</h5>
     <el-row :gutter="12">
       <Letter :letter="letterCN" />
       <Deduction :deduction="deductionCN" />
@@ -78,31 +78,31 @@ export default {
       letterCN: [
         {
           id: 'I',
-          year: '2020',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
+          year: '2021',
+          title: '一页废纸篓中未寄出的信',
+          letter: '尊敬的父亲：\n我窥见了造物的真谛。\n曾经，我还会奢求傻瓜的赞美，\n现在，我想自己无暇顾及那些无聊与琐碎。\n也许这该归功于远游的决定。\n\n还记得我送给德罗斯先生的那件作品吗？\n被关进石中的鸟儿，\n无味，无序，无比渺小以至灵魂逐渐死去。\n是的，它是如此浅薄。\n所以再见之时我亲手砸碎了它。\n\n而在我意识到这一点之前，\n我在这，看到了被豢养的木偶和自诩高贵的奴隶，\n看到了瞻仰遗容就像簇拥国王的蚂蚁，\n看到了河床断裂后被污浊浇透的灵魂，\n……\n我看到了邪恶的伟大。\n\n当我意识到人类不过是在灵魂之上铺砌了一层薄石膏之后，\n我听见满脑子高速旋飞叫嚣的想法，\n我听见躯体的存在本身多么易碎，\n我听见敲碎一枚蛋壳的声音，\n我听见雕像的神性诞生，\n艺术的真理破壳。\n'
         },
         {
           id: 'II',
-          year: '2021',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
+          year: '2022',
+          title: '一页留言',
+          letter: '亲爱的伽拉泰亚:\n你还好吗?\n凌晨回房后你就将自己锁在屋内不出来,也不说话,我们只能听到连续不断但又微弱的敲击声,你在做什么,我很担心你……\n你一定被吓坏了。\n谁也没想到事情会发展成那样,但当时奥尔菲斯先生也是为了自保,为了救我们，才不得不动手。\n就像袭击发生时你第一时间推开了我一样。\n伽拉泰亚,别怕,我会一直在你身边,我会保护你,我们会一起从这里离开。\n如若你心绪安定下来或者有任何需要,请一定要呼唤我。\n\n另外还有一件事,昨晚我们再回到院中时发现他的尸体不见了，但奥尔菲斯先生说并不是他处理的,我有种不太好的预感……\n总之,你独处时请诸事小心。\n海伦娜留'
         },
         {
           id: 'III',
-          year: '2022',
-          title: 'Coming Soon',
-          letter: 'Coming Soon'
-        },
-        {
-          id: 'IV',
           year: '2023',
           title: 'Coming Soon',
           letter: 'Coming Soon'
         },
         {
-          id: 'V',
+          id: 'IV',
           year: '2024',
+          title: 'Coming Soon',
+          letter: 'Coming Soon'
+        },
+        {
+          id: 'V',
+          year: '2025',
           title: 'Coming Soon',
           letter: 'Coming Soon'
         }
