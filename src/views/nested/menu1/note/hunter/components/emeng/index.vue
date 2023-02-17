796d56672b5a02fdf5be8a7454062731486a0763
jiangzy <jiangzy@coscon.com>
2023-02-17 17:05:38 +0800

    0.72 更新部分监管者推演

--- src/views/nested/menu1/note/hunter/components/emeng/index.vue
+++ src/views/nested/menu1/note/hunter/components/emeng/index.vue
@@ -12,72 +12,82 @@ export default {
     return {
       descriptionCN: [
         {
-          role: '',
+          role: '“噩梦”',
           type: '传闻',
-          text: ''
+          text: '或许，只有“奥尔菲斯”了解他，这来自记忆的噩梦。'
         },
         {
-          role: '',
+          role: '“噩梦”',
           type: '故事背景',
-          text: ''
+          text: 'TBD'
         }
       ],
       deductionCN: [
         {
+          role: '“噩梦”',
           id: '1',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '2',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '3',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '4',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '5',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '6',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '7',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '8',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '9',
           title: 'TBD',
           content: 'TBD',
           conclusion: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: '10',
           title: 'TBD',
           content: 'TBD',
@@ -86,30 +96,35 @@ export default {
       ],
       letterCN: [
         {
+          role: '“噩梦”',
           id: 'I',
           year: '2023',
           title: 'TBD',
           letter: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: 'II',
           year: '2024',
           title: 'TBD',
           letter: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: 'III',
           year: '2025',
           title: 'TBD',
           letter: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: 'IV',
           year: '2026',
           title: 'TBD',
           letter: 'TBD'
         },
         {
+          role: '“噩梦”',
           id: 'V',
           year: '2027',
           title: 'TBD',
